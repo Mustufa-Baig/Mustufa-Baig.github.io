345056f05b8ef5c13eb06fb334cc292f8f8be686
Mustufa Baig <140980143+Mustufa-Baig@users.noreply.github.com>
2024-06-08 18:44:56 +0530

    projects section added (alpha)

--- anim.js
+++ anim.js
@@ -39,19 +39,19 @@ var cursor = {
     cursorEnlarged: false,
     $dot: document.querySelector('.cursor-dot'),
     $outline: document.querySelector('.cursor-dot-outline'),
-    
+
     init: function() {
         // Set up element sizes
         this.dotSize = this.$dot.offsetWidth;
         this.outlineSize = this.$outline.offsetWidth;
-        
+
         this.setupEventListeners();
         this.animateDotOutline();
     },
-    
+
     setupEventListeners: function() {
         var self = this;
-        
+
         // Anchor hovering
         document.querySelectorAll('a').forEach(function(el) {
             el.addEventListener('mouseover', function() {
@@ -63,7 +63,7 @@ var cursor = {
                 self.toggleCursorSize();
             });
         });
-        
+
         // Click events
         document.addEventListener('mousedown', function() {
             self.cursorEnlarged = true;
@@ -73,8 +73,8 @@ var cursor = {
             self.cursorEnlarged = false;
             self.toggleCursorSize();
         });
-  
-  
+
+
         document.addEventListener('mousemove', function(e) {
             // Show the cursor
             self.cursorVisible = true;
@@ -86,7 +86,7 @@ var cursor = {
             self.$dot.style.top = self.endY + 'px';
             self.$dot.style.left = self.endX + 'px';
         });
-        
+
         // Hide/show cursor
         document.addEventListener('mouseenter', function(e) {
             self.cursorVisible = true;
@@ -94,7 +94,7 @@ var cursor = {
             self.$dot.style.opacity = 1;
             self.$outline.style.opacity = 1;
         });
-        
+
         document.addEventListener('mouseleave', function(e) {
             self.cursorVisible = true;
             self.toggleCursorVisibility();
@@ -102,21 +102,21 @@ var cursor = {
             self.$outline.style.opacity = 0;
         });
     },
-    
+
     animateDotOutline: function() {
         var self = this;
-        
+
         self._x += (self.endX - self._x) / self.delay;
         self._y += (self.endY - self._y) / self.delay;
         self.$outline.style.top = self._y + 'px';
         self.$outline.style.left = self._x + 'px';
-        
+
         requestAnimationFrame(this.animateDotOutline.bind(self));
     },
-    
+
     toggleCursorSize: function() {
         var self = this;
-        
+
         if (self.cursorEnlarged) {
             self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
             self.$outline.style.transform = 'translate(-50%, -50%) scale(1.8)';
@@ -125,10 +125,10 @@ var cursor = {
             self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
         }
     },
-    
+
     toggleCursorVisibility: function() {
         var self = this;
-        
+
         if (self.cursorVisible) {
             self.$dot.style.opacity = 1;
             self.$outline.style.opacity = 1;
